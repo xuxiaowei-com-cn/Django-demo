110d15cd615854bb6732d26ddd85f45afe7d7d0a
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2021-06-17 00:43:39 +0800

    编写视图-测试API1接口

--- tests/apis/api1/views.py
+++ tests/apis/api1/views.py
@@ -1,8 +1,23 @@
-from django.shortcuts import render
+import json
+
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 
 
 # Create your views here.
 
+@csrf_exempt
 def index(request):
-    """学习笔记的主页"""
-    return render(request, 'tests/apis/api1/index.html')
+    data = {}
+    if request.method == 'GET':
+        data['code'] = 0
+        data['msg'] = '成功'
+        response = HttpResponse(json.dumps(data, ensure_ascii=False))
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    else:
+        data['msg'] = 1
+        data['msg'] = '失败'
+        response = HttpResponse(json.dumps(data, ensure_ascii=False))
+        response.headers['Content-Type'] = 'application/json'
+        return response
